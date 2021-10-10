ade18b0531026a36af4a4773782ae09ac59a8dc1
Aydogdy <aydogdy@Aydogdys-Mac.local>
2021-10-09 21:35:07 -0700

    WindowShopperApp

---
+++ shopperApp/shopperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopperApp
+//
+//  Created by Aydogdy on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
