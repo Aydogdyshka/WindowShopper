8a89b18df02cf5916b3309a70f660075dcd0e3bb
Aydogdy <aydogdy@Aydogdys-Mac.local>
2021-10-14 04:00:39 -0700

    Added button from controller

--- shopperApp/shopperApp/Controller/MainVC.swift
+++ shopperApp/shopperApp/Controller/MainVC.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for:.touchUpInside )
+    
+        wageTxt.inputAccessoryView = calcButton
+        priceTxt.inputAccessoryView = calcButton
+        
+    }
+    
+    @objc func calculate(){
+        print("we got here")
     }
 
     
