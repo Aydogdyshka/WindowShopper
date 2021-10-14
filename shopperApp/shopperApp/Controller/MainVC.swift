c9f78ad68eeee4f2f34a35bf1ee32d77b8359042
Aydogdy <aydogdy@Aydogdys-Mac.local>
2021-10-14 03:17:53 -0700

    Edited Text View

--- shopperApp/shopperApp/Controller/MainVC.swift
+++ shopperApp/shopperApp/Controller/MainVC.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
